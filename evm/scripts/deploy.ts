ccf8cfeb3d67a6469fe3ccf6b4e3a8f98fd7fb15
Arthur Kepler <kepler@partavate.com>
2022-08-02 00:16:16 -0700

    Deploy script

--- evm/scripts/deploy.ts
+++ evm/scripts/deploy.ts
@@ -1,18 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const MulitverseShip = await ethers.getContractFactory("Multiverse");
+  const ship = await MulitverseShip.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await ship.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Ship deployed to:", ship.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
