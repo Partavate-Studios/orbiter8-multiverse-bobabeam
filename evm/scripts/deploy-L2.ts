65c98a7c4a5e867735443bb5a570658bb0dc784a
Arthur Kepler <kepler@partavate.com>
2022-08-08 15:55:16 -0700

    Contract deployment scripts

---
+++ evm/scripts/deploy-L2.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+import { logDeployment } from "../lib/logDeployment"
+
+// Deploys Multiverse, for non-bridge networks
+async function main() {
+  const MulitverseShip = await ethers.getContractFactory("MultiverseShip_L2");
+  const ship = await MulitverseShip.deploy();
+
+  await ship.deployed();
+
+  console.log("L2 MultiverseShip deployed to:", ship.address);
+  let chaindata = await ethers.provider.getNetwork()
+  logDeployment(chaindata.chainId, ship.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
--- evm/scripts/deploy-standard.ts
+++ evm/scripts/deploy-standard.ts
@@ -1,12 +1,16 @@
 import { ethers } from "hardhat";
+import { logDeployment } from "../lib/logDeployment"
 
+// Deploys Multiverse, for non-bridge networks
 async function main() {
-  const MulitverseShip = await ethers.getContractFactory("Multiverse");
+  const MulitverseShip = await ethers.getContractFactory("MultiverseShip");
   const ship = await MulitverseShip.deploy();
 
   await ship.deployed();
 
   console.log("Ship deployed to:", ship.address);
+  let chaindata = await ethers.provider.getNetwork()
+  logDeployment(chaindata.chainId, ship.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
